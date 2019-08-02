18eea1afe841fa3d097c76d58e039905203fc5fa
gamwe6 <gamwe6@users.noreply.github.com>
2019-08-02 21:46:33 +0100

    Modify gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Euler in Babylon`,
+    author: `gamwe6`,
+    description: `Project Euler in different languages.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `gamwe6`,
     },
   },
   plugins: [
